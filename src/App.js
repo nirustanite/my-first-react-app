4c696d876faec092e06053e2f03b7c8937fba352
nirustanite <nirustanite@gmail.com>
2019-09-09 11:38:47 +0200

    lightswitch implementation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Title from "./components/Title";
+import LightSwitch from "./components/LightSwitch";
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <main>
           <Title content ="Hello React"/>
+          <LightSwitch />
         </main>
 
         <header className="App-header">
