23af9546397251926325c795dcf733dc6283a652
nirustanite <nirustanite@gmail.com>
2019-09-09 11:19:17 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Title from "./components/Title";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+        <main>
+          <Title />
+        </main>
+
+        <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to Codassieur</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello World
-        </a>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          Learn React
+          </a>
+        </header>
       </div>
     );
   }
